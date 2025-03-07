d45a358053dcb18edd2728eecfd1a864d67dd77f
Bncity <mtechbro@gmail.com>
2025-03-07 11:37:43 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,399 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-exterior-stair-area",
+      "name": "Exterior stair area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.14345956443481,
+        "pitch": -0.04351375975529592,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21372210493817967,
+          "pitch": 0.06365584146580439,
+          "rotation": 0,
+          "target": "1-table-area"
+        },
+        {
+          "yaw": 0.2874798942522503,
+          "pitch": -0.2906634344982759,
+          "rotation": 0,
+          "target": "5-upstair-bedroom-1"
+        },
+        {
+          "yaw": 1.0229647975504115,
+          "pitch": 0.09190824763320116,
+          "rotation": 5.497787143782138,
+          "target": "3-exterior-pool-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-table-area",
+      "name": "Table area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.1065683992694346,
+        "pitch": 0.09482288781817516,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.109731310657194,
+          "pitch": 0.0932478336808984,
+          "rotation": 4.71238898038469,
+          "target": "2-sofa-area"
+        },
+        {
+          "yaw": -1.4573103435389854,
+          "pitch": 0.1353637931565661,
+          "rotation": 0.7853981633974483,
+          "target": "3-exterior-pool-area"
+        },
+        {
+          "yaw": 1.5171454258812451,
+          "pitch": 0.18347065961853026,
+          "rotation": 5.497787143782138,
+          "target": "0-exterior-stair-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sofa-area",
+      "name": "Sofa area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0015745638895729996,
+          "pitch": -0.13304998199781792,
+          "rotation": 0,
+          "target": "5-upstair-bedroom-1"
+        },
+        {
+          "yaw": -0.8568476482198584,
+          "pitch": -0.03607209728571803,
+          "rotation": 4.71238898038469,
+          "target": "1-table-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-exterior-pool-area",
+      "name": "Exterior pool area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26887977589106526,
+          "pitch": -0.12533538877106665,
+          "rotation": 0,
+          "target": "6-upstair-bedroom"
+        },
+        {
+          "yaw": 0.274727063637501,
+          "pitch": -0.1266248281175386,
+          "rotation": 0,
+          "target": "4-upstair-work-area"
+        },
+        {
+          "yaw": 0.12220618874323108,
+          "pitch": 0.0359300751815006,
+          "rotation": 0,
+          "target": "2-sofa-area"
+        },
+        {
+          "yaw": 0.43355470182706846,
+          "pitch": -0.13211446137361627,
+          "rotation": 0,
+          "target": "5-upstair-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-upstair-work-area",
+      "name": "Upstair work area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5263507534985692,
+          "pitch": 0.15796443648041247,
+          "rotation": 5.497787143782138,
+          "target": "6-upstair-bedroom"
+        },
+        {
+          "yaw": 0.7155231885549611,
+          "pitch": 0.14863749627877532,
+          "rotation": 14.137166941154074,
+          "target": "5-upstair-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-upstair-bedroom-1",
+      "name": "Upstair bedroom 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0038079910952610874,
+        "pitch": 0.3387127252465625,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8511506064680248,
+          "pitch": 0.28197801998036276,
+          "rotation": 4.71238898038469,
+          "target": "0-exterior-stair-area"
+        },
+        {
+          "yaw": 0.888748874452304,
+          "pitch": 0.27812103144133893,
+          "rotation": 1.5707963267948966,
+          "target": "3-exterior-pool-area"
+        },
+        {
+          "yaw": 0.8140755867577916,
+          "pitch": 0.6080701498892758,
+          "rotation": 2.356194490192345,
+          "target": "4-upstair-work-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-upstair-bedroom",
+      "name": "Upstair bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5263507534985692,
+          "pitch": 0.04897359195967965,
+          "rotation": 2.356194490192345,
+          "target": "0-exterior-stair-area"
+        },
+        {
+          "yaw": -0.7837544024748695,
+          "pitch": 0.031216463818662277,
+          "rotation": 7.0685834705770345,
+          "target": "3-exterior-pool-area"
+        },
+        {
+          "yaw": 0.009447276781882508,
+          "pitch": 0.15617013581940498,
+          "rotation": 0,
+          "target": "7-upstair-bedroom-closup"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-upstair-bedroom-closup",
+      "name": "Upstair bedroom Closup",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.43095835922666126,
+          "pitch": 0.06286438433801678,
+          "rotation": 1.5707963267948966,
+          "target": "3-exterior-pool-area"
+        },
+        {
+          "yaw": -0.7261853657214328,
+          "pitch": 0.042359036664013416,
+          "rotation": 4.71238898038469,
+          "target": "0-exterior-stair-area"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Sample work",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
